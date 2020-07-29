c0464b804912f2d1896fd331494fcca7877c722d
Suraj Bansal <bansalsuraj03@gmail.com>
2020-07-29 19:13:36 -0400

    mobile fade effect

--- app.js
+++ app.js
@@ -113,3 +113,17 @@ $(window).on("load",function() {
     });
   }).scroll();
 });
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade-mobile").each(function() {
+      var objectBottom = $(this).offset().top + $(this).outerHeight();      
+      if (objectBottom < windowBottom) { 
+        if ($(this).css("opacity")==0) {$(this).fadeTo(400,1);}
+      } else {
+        if ($(this).css("opacity")==1) {$(this).fadeTo(400,0);}
+      }
+    });
+  }).scroll();
+});
